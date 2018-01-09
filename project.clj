b270ad613be4454e5387ac10e4aa6b48a6b3a390
John Flinchbaugh <john@hjsoft.com>
2018-01-09 01:56:44 -0500

    2018-01-08 report from opml
    getting closer, though still not convenient

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.hjsoft/clojure-scratch "1.0.0-SNAPSHOT"
+  :dependencies [
+    [org.clojure/clojure "1.9.0"]
+    [org.clojure/data.xml "0.0.8"]
+    [org.clojure/data.zip "0.1.1"]
+  ]
+)
