58432f186064f26f16d0e4f57e9e3996390c2ec7
John Flinchbaugh <john@hjsoft.com>
2018-03-14 00:10:04 -0400

    4clj #44

--- 2018-03.clj
+++ 2018-03.clj
@@ -22,3 +22,18 @@
 (map list [0 1 2 3 4] [5 6 7 8 9])
 
 (#(apply map list (partition %2 %1)) (range 10) 5)
+
+; 4clj#44
+
+(fn [rotate input]
+  (let
+    [
+      size (count input)
+      shift (mod (+ size rotate) size)
+      front (take shift input)
+      count-left (- size shift)
+      back (reverse (take count-left (reverse input)))
+    ]
+    (concat back front)
+  )
+)
