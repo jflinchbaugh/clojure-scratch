951a6928207f95ddfd55eaa271161dfe0b88e54d
John Flinchbaugh <john@hjsoft.com>
2018-03-20 20:40:44 -0400

    4clj #88

--- 2018-03.clj
+++ 2018-03.clj
@@ -163,3 +163,18 @@
 
 (= (f #{1 2 3} #{4 5})
    #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})
+
+; 4clj #88
+
+(def f
+  (fn [x y]
+    (set
+      (concat
+        (filter (complement x) y)
+        (filter (complement y) x)
+      )
+    )
+  )
+)
+
+(= (f #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})
