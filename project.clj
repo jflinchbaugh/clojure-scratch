4197cf75515d52f372f967bd89ca8dd9fb3db4b4
John Flinchbaugh <john@hjsoft.com>
2018-10-13 01:09:02 -0400

    upgrade to clojure 1.10.0-beta2

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.hjsoft/clojure-scratch "1.0.0-SNAPSHOT"
   :dependencies [
-    [org.clojure/clojure "1.9.0"]
+    [org.clojure/clojure "1.10.0-beta2"]
     [org.clojure/data.xml "0.0.8"]
     [org.clojure/data.zip "0.1.1"]
   ]
