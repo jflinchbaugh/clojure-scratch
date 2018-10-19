4f279c1f6033eadeaae0ed51024cda687c335c59
John Flinchbaugh <john@hjsoft.com>
2018-10-18 20:22:42 -0400

    exercise in sticking maps together

--- 2018-10.clj
+++ 2018-10.clj
@@ -5,3 +5,39 @@
 (comment
   things
 )
+
+
+(do
+  (prn "hello")
+  (prn "hello")
+  "hello"
+)
+
+; parse, split, shift dates
+
+(def data [
+  { :name "breakfast" :start "1" }
+  { :name "lunch" :start "2" }
+  { :name "dinner" :start "3" }
+])
+
+(defn rotate [lst]
+  (concat
+    (rest lst)
+    (-> lst first str (cons []))))
+
+(defn with-end
+  "rotate the :start to make an :end for each entry"
+  [data]
+  (let [
+    starts (map :start data)
+    ends (rotate starts)]
+    (map #(merge %1 %2) data (map #(hash-map :end %) ends))
+  )
+)
+
+(doc with-end)
+
+(with-end data)
+
+(apropos "drop")
