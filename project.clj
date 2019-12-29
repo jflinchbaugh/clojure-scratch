38c8c7a281239755f714a278d8f2ca9e79a35871
John Flinchbaugh <john@hjsoft.com>
2019-12-28 23:25:29 -0500

    upgrade clojure

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.hjsoft/clojure-scratch "1.0.0-SNAPSHOT"
   :dependencies [
-    [org.clojure/clojure "1.10.0-beta2"]
+    [org.clojure/clojure "1.10.1"]
     [org.clojure/data.xml "0.0.8"]
     [org.clojure/core.logic "0.8.11"]
     [org.clojure/data.zip "0.1.1"]
