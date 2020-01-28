6a9259f9d281c8fd43fb09c8bdc3ebcb9adb94e7
John Flinchbaugh <john@hjsoft.com>
2020-01-28 14:48:58 -0800

    upgrade data.zip

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
     [org.clojure/clojure "1.10.1"]
     [org.clojure/data.xml "0.0.8"]
     [org.clojure/core.logic "0.8.11"]
-    [org.clojure/data.zip "0.1.1"]
+    [org.clojure/data.zip "0.1.3"]
     [hiccup "1.0.5"]
   ]
 )
