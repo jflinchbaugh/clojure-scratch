2c86cd300eed896cdcc1f0fa8062e48e34664ae5
John Flinchbaugh <john@hjsoft.com>
2020-03-19 16:22:57 -0400

    upgrade/add plugins

--- project.clj
+++ project.clj
@@ -2,8 +2,9 @@
   :dependencies [
     [org.clojure/clojure "1.10.1"]
     [org.clojure/data.xml "0.0.8"]
-    [org.clojure/core.logic "0.8.11"]
-    [org.clojure/data.zip "0.1.3"]
+    [org.clojure/data.csv "1.0.0"]
+    [org.clojure/core.logic "1.0.0"]
+    [org.clojure/data.zip "1.0.0"]
     [hiccup "1.0.5"]
   ]
 )
