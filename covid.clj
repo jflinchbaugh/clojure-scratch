d84fb0d49dcde4d93b6b1c53977ca43ec03c49c7
John Flinchbaugh <john@hjsoft.com>
2020-05-19 10:53:36 -0400

    print dates nicely

--- covid.clj
+++ covid.clj
@@ -4,7 +4,8 @@
             [clojure.instant :as instant]
             [java-time :as t]
             [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [clojure.pprint :as pp]))
 
 (defn read-6 [[n2 n3 n4 n5 n6 n7]]
   {:county ""
@@ -32,18 +33,18 @@
        .list
        (filter (partial re-matches #".*\.csv"))
        (pmap
-         (comp
-           rest
-           csv/read-csv
-           io/reader
-           (partial io/file path)))
+        (comp
+         rest
+         csv/read-csv
+         io/reader
+         (partial io/file path)))
        (reduce concat)))
 
 (defn cols->maps [line]
   ((case (count line)
-           6 read-6
-           8 read-8
-           12 read-12) line))
+     6 read-6
+     8 read-8
+     12 read-12) line))
 
 (defn parse-date [s]
   (cond
@@ -55,7 +56,7 @@
     (re-matches #"\d+-\d+-\d+ \d+:\d+:\d+" s) (t/local-date "y-M-d H:m:s" s)
     :else (throw (IllegalArgumentException. (str "Bad date: " s)))))
 
-(defn fix-date [m] (update-in m [:date] parse-date))
+(defn fix-date [m] (update-in m [:date] (comp str parse-date)))
 
 (comment
   (->>
@@ -65,6 +66,7 @@
    (pmap fix-date)
    (filter #(= ["Lancaster" "Pennsylvania"] ((juxt :county :state) %)))
    (sort-by :date)
-   (pmap (juxt :date :cases :deaths)))
+   (pmap (comp (partial str/join " ") (juxt :date :cases :deaths)))
+   (map println))
 
   nil)
