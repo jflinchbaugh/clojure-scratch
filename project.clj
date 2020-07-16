4463ed0599c0fe6683ecf81d6a71f857218415f3
John Flinchbaugh <john@hjsoft.com>
2020-07-16 15:15:42 -0400

    moved src files, added combinatorics

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.csv "1.0.0"]
                  [org.clojure/core.logic "1.0.0"]
+                 [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.zip "1.0.0"]
                  [cljc.java-time "0.1.8"]
                  [clojure.java-time "0.3.2"]
---
+++ src/scratch/2020_07.clj
@@ -0,0 +1,18 @@
+(ns scratch.2020-07
+  (:require [clojure.core.logic :as l]
+            [clojure.math.combinatorics :as combo]))
+
+(def ops [+ - * /])
+(def vals [2 3 3 4])
+
+(comment
+
+  (l/run* [op1 val1 op2 val2 op3 val3 val4]
+    (l/membero op1 ops)
+    (l/membero op2 ops)
+    (l/membero op3 ops)
+    (l/membero [val1 val2 val3 val4] (combo/permutations vals))
+    )
+
+  nil)
+
