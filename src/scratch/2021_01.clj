6c50944cf1f988bde894677f1d677b0b7557d052
John Flinchbaugh <john@hjsoft.com>
2021-01-03 14:03:02 -0500

    SICP making change with tree recursion
    memoize for performance

--- src/scratch/2021_01.clj
+++ src/scratch/2021_01.clj
@@ -19,5 +19,27 @@
         g (partial ackermann 1)
         h (partial ackermann 2)]
     (map (juxt f g h) (range 5)))
-  
+
+  .)
+
+(def first-denomination {1 1, 2 5, 3 10, 4 25, 5 50})
+
+(def cc (memoize (fn [amount kinds-of-coins]
+                    (cond
+                      (= amount 0) 1
+                      (or (< amount 0) (= kinds-of-coins 0)) 0
+                      :else (+
+                              (cc
+                                amount
+                                (dec kinds-of-coins))
+                              (cc
+                                (- amount (first-denomination kinds-of-coins))
+                                kinds-of-coins))))))
+
+(defn count-change [amount] (cc amount 5))
+
+(comment
+
+  (time (count-change 2000))
+
   .)
