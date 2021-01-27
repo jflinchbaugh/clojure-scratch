fdd4aaaa522c4eb6d96c49bed95fb6bb808cba02
John Flinchbaugh <john@hjsoft.com>
2021-01-27 00:25:08 -0500

    upgrade everything
    including clojure 1.10.2

--- project.clj
+++ project.clj
@@ -1,15 +1,15 @@
 (defproject com.hjsoft/clojure-scratch "1.0.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.10.1"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/data.xml "0.0.8"]
                  [org.clojure/data.csv "1.0.0"]
                  [org.clojure/core.logic "1.0.0"]
                  [org.clojure/math.combinatorics "0.1.6"]
                  [org.clojure/data.zip "1.0.0"]
-                 [cljc.java-time "0.1.8"]
+                 [cljc.java-time "0.1.15"]
                  [clojure.java-time "0.3.2"]
                  [hiccup "1.0.5"]
-                 [seancorfield/next.jdbc "1.0.424"]
+                 [seancorfield/next.jdbc "1.1.613"]
                  [com.h2database/h2 "1.4.200"]
                  [hugsql-next-jdbc "0.1.3"]
-                 [clj-commons/clj-yaml "0.7.0"]
-                 [clj-http "3.10.3"]])
+                 [clj-commons/clj-yaml "0.7.2"]
+                 [clj-http "3.11.0"]])
