dee6b91a8da94eaabc1671ba70b6da4b4d2399b3
John Flinchbaugh <john@hjsoft.com>
2022-07-16 13:49:16 -0400

    flatten implementation for 4clojure

---
+++ src/scratch/2022_07.clj
@@ -0,0 +1,32 @@
+(ns scratch.2022-07
+  (:require [clojure.test :as t]))
+
+(defn fltn
+  ([c] (fltn [] c))
+  ([a c]
+   (cond
+     (empty? c) a
+     (coll? (first c)) (fltn a (concat [] (first c) (rest c)))
+     :else (fltn (conj a (first c)) (rest c)))))
+
+(comment
+  ;; https://4clojure.oxal.org/#/problem/28
+  (t/testing
+   (t/is (= '(1 2 3 4 5 6) (fltn '((1 2) 3 [4 [5 6]]))))
+    (t/is (= '("a" "b" "c") (fltn ["a" ["b"] "c"])))
+    (t/is (= '(:a) (fltn '((((:a))))))))
+
+  (t/testing
+   (t/is (= [] (fltn '())))
+    (t/is (= [1 2 3] (fltn '(1 2 3))))
+    (t/is (= [1 2 3] (fltn '((1 2 3)))))
+    (t/is (= [1 2 3] (fltn '(((1 2 3))))))
+    (t/is (= [1 2 3] (fltn '((1) 2 3))))
+    (t/is (= [1 2 3] (fltn '(1 (2) 3))))
+    (t/is (= [1 2 3] (fltn '(1 (2 3)))))
+    (t/is (= [1 2 3 4] (fltn '((1 2) (3 4)))))
+    )
+
+  (fltn [:a :b :c])
+
+  nil)
