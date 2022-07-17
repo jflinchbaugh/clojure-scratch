30e1b137728c44576a3432ea9438f364c40744f4
John Flinchbaugh <john@hjsoft.com>
2022-07-17 11:05:44 -0400

    4clojure

--- src/scratch/2022_07.clj
+++ src/scratch/2022_07.clj
@@ -25,19 +25,62 @@
     (t/is (= [1 2 3] (fltn '((1) 2 3))))
     (t/is (= [1 2 3] (fltn '(1 (2) 3))))
     (t/is (= [1 2 3] (fltn '(1 (2 3)))))
-    (t/is (= [1 2 3 4] (fltn '((1 2) (3 4)))))
-    )
+    (t/is (= [1 2 3 4] (fltn '((1 2) (3 4))))))
 
   (fltn [:a :b :c])
 
   nil)
 
 (def only-caps (fn [s]
-  (str/join (filter (comp (partial re-matches #"[A-Z]") str) s))))
+                 (str/join (filter (comp (partial re-matches #"[A-Z]") str) s))))
 
 (comment
-  (t/is (= "HLOWRD" (only-caps "HeLlO, WoRlD!")))
+  (t/is (= "HLOWRD" (only-caps "HeLlO, WoRlD!")))    (= (__ [1 1 2 1 1 1 3 3]) '((1 1) (2) (1 1 1) (3 3)))
 
+  (= (__ [:a :a :b :b :c]) '((:a :a) (:b :b) (:c)))
 
+  (= (__ [[1 2] [1 2] [3 4]]) '(([1 2] [1 2]) ([3 4])))
 
   nil)
+
+(def compress (fn [c]
+                (reduce
+                 (fn [a i] (if (= (last a) i) a (conj a i)))
+                 []
+                 c)))
+
+(comment
+  (t/testing
+   (t/is (= "Leroy" (apply str (compress "Leeeeeerrroyyy"))))
+    (t/is (= '(1 2 3 2 3) (compress [1 1 2 3 3 2 2 3])))
+    (t/is (= '([1 2] [3 4] [1 2]) (compress [[1 2] [1 2] [3 4] [1 2]]))))
+
+  .)
+
+(def pack
+  (fn [c]
+    (partition-by identity c)))
+
+(comment
+  (t/testing
+   (t/is (= '((1 1) (2) (1 1 1) (3 3)) (pack [1 1 2 1 1 1 3 3])))
+    (t/is (= '((:a :a) (:b :b) (:c)) (pack [:a :a :b :b :c])))
+    (t/is (= '(([1 2] [1 2]) ([3 4])) (pack [[1 2] [1 2] [3 4]]))))
+
+  .)
+
+(def double-seq
+  (fn [c] (reduce (fn [a i] (conj a i i)) [] c)))
+
+(comment
+  (double-seq [1 2 3])
+
+  .)
+
+(def dup-seq
+  (fn [c n] (reduce (fn [a i] (apply conj a (repeat n i))) [] c)))
+
+(comment
+  (dup-seq [1 2 3] 3)
+
+  .)
