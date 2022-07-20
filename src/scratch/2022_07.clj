5968734ba938b3ecbc7ceb31bc60f2ed72492f73
John Flinchbaugh <john@hjsoft.com>
2022-07-20 10:51:43 -0400

    4clojure

--- src/scratch/2022_07.clj
+++ src/scratch/2022_07.clj
@@ -13,12 +13,12 @@
 (comment
   ;; https://4clojure.oxal.org/#/problem/28
   (t/testing "fltn"
-   (t/is (= '(1 2 3 4 5 6) (fltn '((1 2) 3 [4 [5 6]]))))
+    (t/is (= '(1 2 3 4 5 6) (fltn '((1 2) 3 [4 [5 6]]))))
     (t/is (= '("a" "b" "c") (fltn ["a" ["b"] "c"])))
     (t/is (= '(:a) (fltn '((((:a))))))))
 
   (t/testing "fltn"
-   (t/is (= [] (fltn '())))
+    (t/is (= [] (fltn '())))
     (t/is (= [1 2 3] (fltn '(1 2 3))))
     (t/is (= [1 2 3] (fltn '((1 2 3)))))
     (t/is (= [1 2 3] (fltn '(((1 2 3))))))
@@ -51,7 +51,7 @@
 
 (comment
   (t/testing "compress"
-   (t/is (= "Leroy" (apply str (compress "Leeeeeerrroyyy"))))
+    (t/is (= "Leroy" (apply str (compress "Leeeeeerrroyyy"))))
     (t/is (= '(1 2 3 2 3) (compress [1 1 2 3 3 2 2 3])))
     (t/is (= '([1 2] [3 4] [1 2]) (compress [[1 2] [1 2] [3 4] [1 2]]))))
 
@@ -63,7 +63,7 @@
 
 (comment
   (t/testing "pack"
-   (t/is (= '((1 1) (2) (1 1 1) (3 3)) (pack [1 1 2 1 1 1 3 3])))
+    (t/is (= '((1 1) (2) (1 1 1) (3 3)) (pack [1 1 2 1 1 1 3 3])))
     (t/is (= '((:a :a) (:b :b) (:c)) (pack [:a :a :b :b :c])))
     (t/is (= '(([1 2] [1 2]) ([3 4])) (pack [[1 2] [1 2] [3 4]]))))
 
@@ -91,8 +91,8 @@
      (rng [] a b))
     ([c a b]
      (if
-         (>= a b) c
-         (recur (conj c a) (inc a) b)))))
+      (>= a b) c
+      (recur (conj c a) (inc a) b)))))
 
 (comment
 
@@ -124,15 +124,15 @@
 (def drop-nth
   (fn [c n]
     (mapcat
-      (fn [v i] (if (= i n) [] [v]))
-      c
-      (cycle (range 1 (inc n))))))
+     (fn [v i] (if (= i n) [] [v]))
+     c
+     (cycle (range 1 (inc n))))))
 
 (comment
 
   (t/testing "drop-nth"
     (t/is (= [1 3 5] (drop-nth [1 2 3 4 5 6] 2)))
-    (t/is (= [1 3 5] (drop-nth [1 2 3 4 5 ] 2))))
+    (t/is (= [1 3 5] (drop-nth [1 2 3 4 5] 2))))
 
   .)
 
@@ -145,23 +145,22 @@
 (comment
 
   (t/testing "fac"
-      (t/is (= 1 (fac 1)))
+    (t/is (= 1 (fac 1)))
     (t/is (= 6 (fac 3)))
     (t/is (= 120 (fac 5)))
     (t/is (= 40320 (fac 8))))
-
 
   .)
 
 (def rev-intlv
   (fn [c n]
     (map (fn [l] (map first l))
-      (vals
-        (group-by second
-          (map
-            (fn [v i] [v i])
-            c
-            (cycle (range 1 (inc n)))))))))
+         (vals
+          (group-by second
+                    (map
+                     (fn [v i] [v i])
+                     c
+                     (cycle (range 1 (inc n)))))))))
 
 (comment
   (t/testing "rev-intlv"
@@ -185,5 +184,75 @@
     (t/is (= '(2 3 4 5 1) (rotate 6 [1 2 3 4 5])))
     (t/is (= '(:b :c :a) (rotate 1 '(:a :b :c))))
     (t/is (= '(:c :a :b) (rotate -4 '(:a :b :c)))))
+
+  .)
+
+(def longest-sub-seq
+  (fn [c]
+    (let [increasing (->> c
+                          (partition 2 1)
+                          (partition-by (partial apply <))
+                          (filter (fn [i] (apply < (first i))))
+                          (map
+                           (fn [i]
+                             (concat
+                              [(ffirst i)]
+                              (map second i)))))
+          length (apply max (cons 0 (map count increasing)))]
+      (->> increasing
+        (cons [])
+        (filter (fn [i] (= length (count i))))
+        first))))
+
+(comment
+
+  (t/testing "longest-sub-seq"
+    (t/is (= [0 1 2 3] (longest-sub-seq [1 0 1 2 3 0 4 5])))
+    (t/is (= [5 6] (longest-sub-seq [5 6 1 3 2 7])))
+    (t/is (= [3 4 5] (longest-sub-seq [2 3 3 4 5])))
+    (t/is (= [] (longest-sub-seq [7 6 5 4]))))
+
+  .)
+
+(def prtn
+  (fn prtn [n c]
+    (if (> n (count c)) []
+        (concat [(take n c)] (prtn n (drop n c))))
+    ))
+
+(comment
+  (t/testing "partition"
+    (t/is (= '((0 1 2) (3 4 5) (6 7 8)) (prtn 3 (range 9))))
+    (t/is (= '((0 1) (2 3) (4 5) (6 7)) (prtn 2 (range 8))))
+    (t/is (= '((0 1 2) (3 4 5)) (prtn 3 (range 8)))))
+
+
+  .)
+
+(def freq
+  (fn [c]
+    (->> c (group-by identity) (map (fn [[k v]] [k (count v)])) (into {}))))
+(comment
+  (t/is (= {:a 2, :b 3} (freq [:b :a :b :a :b])))
+
+  .)
+
+(def dedup
+  (fn [c]
+    (reduce (fn [a i] (if (some #{i} a) a (conj a i))) [] c)
+    ))
+
+(comment
+
+  (t/is (= [:a :b :c] (dedup [:a :a :b :b :c :c])))
+
+  (dedup [1 2 3])
+
+  (#{1} 2)
+
+  (type #{})
+
+
+  ((fn foo [x] (when (> x 0) (conj (foo (dec x)) x))) 5)
 
   .)
